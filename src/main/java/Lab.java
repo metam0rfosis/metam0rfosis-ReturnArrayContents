837feefc0deef2d2716885fd6639b2f8d6f28fd2
metam0rfosis <gregory.khanykov@gmail.com>
2023-02-13 19:52:07 +0000

    All tests pass

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,13 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+        String result = nums[0] + "";
+        for (int i = 1; i < nums.length; i++) {
+            result += " " + nums[i];
+        }
+        return result;
     }
 }
